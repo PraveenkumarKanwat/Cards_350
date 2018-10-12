18eb8e6e5e4e1d2cfd6fadc44f87f9ecd408e651
ezfrigate <41564901+ezfrigate@users.noreply.github.com>
2018-10-12 20:22:26 +0530

    test
    check this out!

---
+++ src/main/java/App.java
@@ -0,0 +1,15 @@
+package com.game.Cards350;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    		Deck deck = new Deck();
+    		deck.Shuffle();
+    		deck.showDeck();
+    }
+}
