aa38c463f4ad11649e1895fdcf1210ff5af24761
Swapnil Bole <swapnil.bole@schneider-electric.com>
2018-10-15 13:43:28 +0530

    removing dupe dir :O

--- src/main/java/game/Play.java
+++
@@ -1,9 +0,0 @@
-package game;
-
-public class Play {
-    public static void main(String[] args) {
-        Deck deck = new Deck();
-        deck.shuffleDeck();
-        deck.showDeck();
-    }
-}
--- src/main/java/game/Rank.java
+++
@@ -1,4 +0,0 @@
-package game;
-
-public enum Rank {
-}
