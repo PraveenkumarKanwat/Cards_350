42f15af4b1a1767d02bbc35ac0274df6ea9ee904
Praveenkumar Kanwat <pkanwat@dataxu.com>
2018-12-10 01:26:05 +0530

    More additions in code of Bidding and Partner

--- GamePlay/src/test/java/cards/game/BiddingTest.java
+++ GamePlay/src/test/java/cards/game/BiddingTest.java
@@ -1,18 +1,34 @@
 package cards.game;
 
+import java.util.ArrayList;
+
 import cards.basic.Card;
+import cards.basic.Hand;
 import cards.basic.Rank;
 import cards.basic.Suit;
 
 public class BiddingTest {
 
     public static void main(String[] args) {
-//        Bidding bidding = new Bidding();
-//        bidding.doBidding();
-//        System.out.println(Rank.getRankBySymbol("o"));
-        String input = "JS";
-        Rank r = Rank.getRankBySymbol(input.substring(0,input.length() - 1));
-        Suit s = Suit.getSuitByString(input.substring(input.length() - 1));
-        Card card = new Card(s,r);
+        ArrayList<Player> players = new ArrayList<Player>();
+
+        players.add(new Player(1,"Praveen",new Hand()));
+        players.add(new Player(2,"GV",new Hand()));
+        players.add(new Player(3,"Sai",new Hand()));
+        players.add(new Player(4,"Shetty",new Hand()));
+        players.add(new Player(5,"Bole",new Hand()));
+        players.add(new Player(6,"Appu",new Hand()));
+
+        Bidding bidding = new Bidding(players);
+        int hb = bidding.getHighestBidderPosition();
+        System.out.println("HB = " + hb);
+
+
+
+
+        //        String input = "JS";
+//        Rank r = Rank.getRankBySymbol(input.substring(0,input.length() - 1));
+//        Suit s = Suit.getSuitByString(input.substring(input.length() - 1));
+//        Card card = new Card(s,r);
     }
 }
--- GamePlay/src/test/java/cards/game/PartnerTest.java
+++ GamePlay/src/test/java/cards/game/PartnerTest.java
@@ -3,7 +3,7 @@ package cards.game;
 public class PartnerTest {
 
     public static void main(String[] args) {
-        Partner partner = new Partner();
-        partner.callPartner();
+//        Partner partner = new Partner();
+//        partner.callPartner();
     }
 }
